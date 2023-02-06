e5bb6a63a3ac193389fd342635ae556f646263ef
inter <test@test.test>
2023-02-06 10:18:11 +0900

    c

---
+++ fourGreen/src/main/java/com/green/intercepter/AuthCheckInterceptor.java
@@ -0,0 +1,51 @@
+package com.green.intercepter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AuthCheckInterceptor implements HandlerInterceptor{
+	
+				//어댑터 패턴 / 디폴트 메서드
+	//인터셉터를 적용하기 위해서 
+	//1. 어떤 컨트롤러에 적용할지
+	//2. 어떤 인터셉터를 적용할지
+	
+	@Override //로그인 되어 있는지 확이하여 처리 컨트롤러 수행되기 전 구동됨
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// 컨트롤러가 수행되기 전 구동되는 메서드 => 로그인 인증
+		
+		HttpSession session = request.getSession();
+		if(session != null) {
+			Object obj = session.getAttribute("authInfo");
+			if(obj != null) {
+				return true;
+			}
+		}
+		
+		response.sendRedirect(request.getContextPath()+"/login");
+		
+		return false;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// 컨트롤러가 수행된 후 / 뷰페이지가 작동되기 전 구동되는 메서드
+		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		// 뷰페이지까지 보여주고 난 후에 구동되는 메서드,
+		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	}
+	
+	
+	
+}
